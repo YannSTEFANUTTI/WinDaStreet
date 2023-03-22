c9b2d5e683ba96f2c40af02cfd58888953b22d6a
YannSTEFANUTTI <stefanutti.yann@gmail.com>
2023-03-22 13:29:17 +0100

    async done

--- backend/src/controllers/friendController.js
+++ backend/src/controllers/friendController.js
@@ -1,34 +1,39 @@
+/* eslint-disable consistent-return */
 const friendModel = require("../models/friendModel");
 
 const friendController = {
   // Get all friends
-  getAllFriends: (req, res, next) => {
-    const { id } = req.params;
-    friendModel
-      .findAll(id)
-      .then((friend) => res.send(friend))
-      .catch((err) => next(err));
+  getAllFriends: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const friends = await friendModel.findAll(id);
+      res.send(friends);
+    } catch (err) {
+      next(err);
+    }
   },
 
   // Add a new friend
-  addNewFriend: (req, res, next) => {
-    friendModel
-      .postNewFriend(req.body)
-      .then((result) => res.status(201).send({ id: result.insertId }))
-      .catch((err) => next(err));
+  addNewFriend: async (req, res, next) => {
+    try {
+      const result = await friendModel.postNewFriend(req.body);
+      res.status(201).send({ id: result.insertId });
+    } catch (err) {
+      next(err);
+    }
   },
 
   // Delete a friend
-  deleteFriend: (req, res, next) => {
-    friendModel
-      .deleteFriend(req.body)
-      .then((result) => {
-        if (result.affectedRows !== 1) {
-          return res.status(404).send(`Friend not found`);
-        }
-        return res.status(200).send(`Friend deleted`);
-      })
-      .catch((err) => next(err));
+  deleteFriend: async (req, res, next) => {
+    try {
+      const result = await friendModel.deleteFriend(req.body);
+      if (result.affectedRows !== 1) {
+        return res.status(404).send(`Friend not found`);
+      }
+      return res.status(200).send(`Friend deleted`);
+    } catch (err) {
+      next(err);
+    }
   },
 };
 
--- backend/src/controllers/scoreControllers.js
+++ backend/src/controllers/scoreControllers.js
@@ -1,77 +1,72 @@
+/* eslint-disable consistent-return */
 const scoreModel = require("../models/scoreModel");
 
 // Create a new score
-const createScore = (req, res, next) => {
-  const { id } = req.params;
-  scoreModel
-    .createScore(0, id)
-    .then((score) => {
-      res.status(201).json(score);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-      next(err);
-    });
+const createScore = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const score = await scoreModel.createScore(0, id);
+    res.status(201).json(score);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    next(err);
+  }
 };
 
 // Get a score
-const getScore = (req, res, next) => {
-  const { id } = req.params;
-  scoreModel
-    .getOneScore(id)
-    .then((score) => {
-      if (!score) {
-        return res.status(404).json({ error: "Score non trouvé !" });
-      }
-      return res.status(200).json(score);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-      next(err);
-    });
+const getScore = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const score = await scoreModel.getOneScore(id);
+    if (!score) {
+      return res.status(404).json({ error: "Score non trouvé !" });
+    }
+    return res.status(200).json(score);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    next(err);
+  }
 };
 
 // Get all scores
-const getAllScore = (req, res, next) => {
-  scoreModel
-    .getAllScore()
-    .then((score) => {
-      if (!score) {
-        return res.status(404).json({ error: "Score non trouvé !" });
-      }
-      return res.status(200).json(score);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-      next(err);
-    });
+const getAllScore = async (req, res, next) => {
+  try {
+    const score = await scoreModel.getAllScore();
+    if (!score) {
+      return res.status(404).json({ error: "Score non trouvé !" });
+    }
+    return res.status(200).json(score);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    next(err);
+  }
 };
 
 // Update a score
-const incrementScore = (req, res, next) => {
-  const { id } = req.params;
-  const { Score } = req.body;
-  scoreModel
-    .incrementScore(id, Score)
-    .then((score) => {
-      if (!score) {
-        return res.status(404).json({ error: "Score non trouvé !" });
-      }
-      return res.status(200).json(score);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-      next(err);
-    });
+const incrementScore = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { Score } = req.body;
+    const score = await scoreModel.incrementScore(id, Score);
+    if (!score) {
+      return res.status(404).json({ error: "Score non trouvé !" });
+    }
+    return res.status(200).json(score);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    next(err);
+  }
 };
 
 // Delete a score
-const deleteScore = (req, res, next) => {
-  const { id } = req.params;
-  scoreModel
-    .deleteOne(id)
-    .then((result) => res.send(result))
-    .catch((err) => next(err));
+const deleteScore = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await scoreModel.deleteOne(id);
+    res.send(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
